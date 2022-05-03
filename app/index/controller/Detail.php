8a456a54f9f3d0f0b9622d4a2fc12ba37dcccf83
autofelix <autofelix@163.com>
2022-05-03 23:19:39 +0800

    演员排序

--- app/index/controller/Detail.php
+++ app/index/controller/Detail.php
@@ -3,6 +3,10 @@ declare (strict_types=1);
 
 namespace app\index\controller;
 
+use app\model\Directors;
+use app\model\Genres;
+use app\model\Producers;
+use app\model\Publishers;
 use think\Request;
 
 use think\facade\View;
@@ -24,7 +28,12 @@ class Detail
         }
 
         $similars = Movies::whereIn('sdde', $movie->similars)->select();
+        $stars = Stars::whereIn('hash', $movie->stars)->where(['type' => $movie->type])->select();
+        $genres = Genres::whereIn('hash', $movie->genres)->where(['type' => $movie->type])->select();
+        $publisher = Publishers::where(['hash' => $movie->publisher])->find();
+        $producer = Producers::where(['hash' => $movie->producers])->find();
+        $director = Directors::where(['hash' => $movie->director])->find();
 
-        return View::fetch('index@detail', compact('movie', 'magnetics', 'similars'));
+        return View::fetch('index@detail', compact('movie', 'magnetics', 'similars', 'stars', 'genres', 'publisher', 'producer', 'director'));
     }
 }
